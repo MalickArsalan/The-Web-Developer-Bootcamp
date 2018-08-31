8dc168fb31fd27b16931c3fde8dfb239a503015d
Malick Arsalan Office <malickarsalan@hotmail.com>
2018-08-31 18:23:46 +0500

    Introduction to JS and Branching complete

---
+++ 6. Introduction to Javascript/3. Null and Undefined.js
@@ -0,0 +1,14 @@
+/*
+Null and Undefined
+
+// The other two primitives are null and undefined
+
+//Variables that are declared but not initialized are undefined
+//The following variables are undefined:
+*/ 
+var name;
+var age;
+
+// null is "explicitly nothing"
+var currentPlayer = "Charlie";
+currentPlayer = null; //game over
---
+++ 6. Introduction to Javascript/4. builtInMethods.js
@@ -0,0 +1,7 @@
+/*
+Built In Methods
+1. alert()
+2. console.log()
+3. prompt() -> It let us get input from user
+4. clear() -> clear the console
+*/
